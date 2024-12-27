51d2be537ff012a6dd5326c6f28190f8e4f76ee6
nathalia <nath.ramosaparecida@gmail.com>
2024-12-27 10:38:14 -0300

    fix: corrigindo nomenclatura na entidade de categorias

--- src/entities/categories.ts
+++ src/entities/categories.ts
@@ -1,19 +1,19 @@
 import { Column, Entity, Index, ManyToOne, PrimaryGeneratedColumn } from "typeorm";
 
-@Entity({name: "users"})
+@Entity({name: "categories"})
 @Index(["id"], {unique: true})
 export class UserEntity {
     @PrimaryGeneratedColumn("increment")
-        id!: number;
+    	id!: number;
 
     @Column({ type: "varchar", length: 180, name: "name", default: null, nullable: false})
-        full_name!: string;
+    	full_name!: string;
     
     @Column({ type: "int", name: "user_id", nullable: false})
-        user_id!: string;
+    	user_id!: string;
 
     @ManyToOne(() => UserEntity, (user) => user.id)
     
-    private user!: UserEntity
+    private user!: UserEntity;
     
 }
